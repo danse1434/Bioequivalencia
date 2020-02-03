993b568f83cdddd74cf4466417d2b6f3f261a995
Daniel Parra <59803596+kdcloop@users.noreply.github.com>
2020-02-03 11:18:35 -0500

    Actualización 1
    Actualización a escritura de lenguaje moderno

--- Clases/plug_flow/solucion_3d.R
+++ Clases/plug_flow/solucion_3d.R
@@ -1,72 +1,116 @@
-# Gráficos 3D con modelo plug-flow
-
-
+##------------------------------------------------------------------------#
+## Nombre del Script: Estudio de modelamiento de Plug-Flow ----------------
+##  
+## Proposito del Script: realizar un estudio del modelo Plug-Flow para enten
+## der la relevancia del numero de dosis, número de absorcion, etc..
+##
+## Gráficos 3D con modelo plug-flow
+##  
+## Autor: Daniel S. Parra Gonzalez 
+## Fecha de creacion:  24-10-2019 
+##  
+## Copyright (c) Daniel S. Parra, 2018 
+##  
+## Email: dsparrag@unal.edu.co 
+##------------------------------------------------------------------------#
+##########################################################################-
+# Apertura de paquetes
 library(rgl) # Se abre un paquete útil para realiza gráficos en 3D
 library(deSolve)
-library(ggplot2)
+library(tidyverse)
+library(Rcpp)
 
-An = 5 # Valores Iniciales
-Dn = 1.0
+##########################################################################-
+# Definición de valores iniciales
+An = 5.0 # N. de absorción
+Dn = 1.0 # N. de dosis
 
-f = function(Dn,An) # Función de Biodisponibilidad
-{ 
-  r_init  = c(rep(1,100))
-  c_init = c(rep(0,100))
+##########################################################################-
+# Función de Biodisponibilidad --------------------------------------------
+##########################################################################-
+##:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+##  1 Especificar funcion (g) para obtener biodisponibilidad teniendo en 
+##  cuenta numeros adimensionales Dn, An, Do
+##  2 Escribir sistema EDO para modelo plug and flow, escribir en objeto 
+##  *mod1* que actua como funcion que tiene como parametros coord, y, parms
+##  3 Escribir las condiciones iniciales, r: diametro de particula, y C: es
+##  concentracion. En el tiempo = 0 se tiene que r es 1, y C es 0.
+##  4 Escribir parametros de modelo Dn, Do, y An
+##  5 Escribir sistemas de coordenadas para modelar en el sistema ODE
+##  6 Modelar con la funcion ode del paquete deSolve
+##  7 Escribir los resultados en forma de tabla
+##  8 Modificar la tabla como numeros, y calcular F que es la biodisponibi-
+##  lidad calculada (F)
+##  9 Retornar como resultado principal, el último valor de F obtenido en la
+##  base de datos.
+##:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+f = function(Dn, An, Do) {
+  mod1 <- function(coord, y , parms) {
+    with(as.list(c(y, parms)), {
+      dr = -(Dn / 3) * ((1 - C) / r) * C
+      dC = (Dn * Do * r * (1 - C)) - (2 * An * C)
+      return(list(c(dr, dC)))
+    })
+  }
+  # Condiciones iniciales
+  y <- c(r = 1, C = 0)
+  # Parámetros del modelo
+  parms <- c(Dn = Dn, Do = Do, An = An)
+  # Sistema de coordenada
+  coord <- seq(0, 1, 0.01)
   
-  Dn = Dn
-  An = An
-  Do = 2
-mod1 <- function(coord, y , parms){
-  with(as.list(c(y, parms)),{
-    dr = - (Dn/3)*((1-C)/r)
-    dC = (Dn*Do*r*(1-C))-(2*An*C)
-    return(list(c(dr, dC)))
-  })
-}
-y <- c(r = 1, C = 0)
-parms <- c(Dn = Dn, Do = Do,An = An)
-coord <- seq(0,1, 0.01)
-out <- ode(y, coord, mod1, parms)
+  # Modelamiento de ecuaciones diferenciales ordinarias
+  out <- deSolve::ode(y = y, times = coord, func = mod1, parms = parms)
 
-results = data.frame(coord = as.numeric(out[,"time"]),
-                     r = as.numeric(out[,"r"]),
-                     C = as.numeric(out[,"C"]))
-
-results$F = as.numeric(1-((results[,"r"])^3)-(results[,"C"]/Do))
-
-  return(tail(results$F,n=1))
-
-rm(y,parms, coord, out,)
-
+  results = data.frame(coord = out[, "time"],
+                       r = out[, "r"],
+                       C = out[, "C"])
+  
+  # Resultados de Biodisponibilidad
+  results <- results %>%
+    dplyr::mutate_all(as.numeric) %>% 
+    dplyr::mutate(F = 1 - r ^ 3 - (C / Do))
+  
+  results %>% 
+    magrittr::use_series("F") %>% 
+    tail(., n = 1) %>% 
+    return(.)
 }
 
-f(An = 5, Dn = 1.0) # Prueba de Función 1
+# microbenchmark::microbenchmark(f(An = 5, Dn = 1.0, Do = 2))
 
-An = seq(0.5,6,length=100) # Generador de malla eje X
-Dn = seq(0.1,10,length=100) # Generador de malla eje Y
-outer1 = expand.grid(An, Dn) # Generador de Combinatorios malla
+##########################################################################-
+# Gráfico de contorno -----------------------------------------------------
+##########################################################################-
+##:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+##  1 Escribir una tabla con valores en escala logarítmica
+##  2 Transformar los valores de parametros a escala ordinaria
+##  3 Vectorizar la funcion f en los parametros a explorar, en este caso Do,
+##  y Dn.
+##  4 Realizar un producto outer entre los dos vectores, para conocer todas
+##  las combinaciones. Calcular el parametro f para cada combinacion de 
+##  parametros.
+##  5 Abrir entorno 3D, y abrir la perspectiva 3D
+##:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-n = dim(outer1)[1] # Dimensiones Totales
+dx = data.frame(
+  log_Dn = seq(-2, 2, length = 1e1), 
+  log_Do = seq(-2, log10(500), length = 1e1))
 
-for (i in 1:n) { # Generador de Respuesta en Malla
-  outer1[i,3] <- f(An = outer1[i,1], Dn = outer1[i,2])
-}
+dx <- dx %>%
+  mutate(Dn = 10 ^ log_Dn,
+         Do = 10 ^ log_Do)
 
-outer2 <- na.omit(outer1, 3) # Eliminación de valores no convergentes
-plot3d(outer2[,1],outer2[,2],outer1[,3],
-       front="line",
-       back="line",
-       add=TRUE,
-       xlab="An",
-       ylab="Dn",
-       zlab="Biodisponibilidad (F)",
-       col="red",
-       ticktype="detailed",
-       aspect="iso") # Gráfico Final de Resultados
-# Animación y toma de fotos
-movie3d(spin3d(axis = c(0,0,1), rpm = 10), duration=6,  type = "png")
-tempdir()   
+f <- Vectorize(f, vectorize.args = c('Do', 'Dn'))
 
+OUT <- outer(dx$Dn, dx$Do, f, An = 2)
 
+open3d()
+persp3d(x = dx$Dn, y = dx$Do, z = OUT,
+        front = "line", back = "line", 
+        xlab = 'Dn', ylab = 'Do')
+rgl.postscript('persp3d.pdf', 'pdf')
 
-
+# Gráfico de contorno
+contour(An, Dn, OUT)
